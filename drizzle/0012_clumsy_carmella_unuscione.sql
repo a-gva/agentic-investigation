f328ec1e0f28077cfe9f0c4661021707c6293d8f
Alexy Viegas <alexyviegas@gmail.com>
2026-06-17 00:02:42 -0300

    Update Congress Press schema and journal with new entries
    - Added a new entry to the journal for tracking the addition of a new member type.
    - Modified the `loadCongressPress` table schema to make the `date` and `text` fields optional, enhancing flexibility in data handling.

---
+++ drizzle/0012_clumsy_carmella_unuscione.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "load_congress_press" ALTER COLUMN "date" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "load_congress_press" ALTER COLUMN "date_source" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "load_congress_press" ALTER COLUMN "text" DROP NOT NULL;
